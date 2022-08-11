e080e17b63fbd514ede702e914637967c8e1832c
carter_383 <carter.383@outlook.com>
2022-08-11 06:54:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,54 @@
+Useful Python Scripts
+
+time_date_util.py:
+
+    get_now:
+    returns current time date as datetime
+    
+    get_date:
+    returns date as text format 'DD:MM:YYYY'
+
+    get_time:
+    returns time as text format 'HH:MM:SS'
+
+    get_day(date(default = now)):
+    returns day as text format 'DD'
+
+    get_month(date(default = now)):
+    returns month as text format 'MM'
+
+    get_year(date(default = now)):
+    returns year as text format 'YYYY'
+
+    get_hour(time(default = now)):
+    returns hour as text format 'HH'
+
+    get_minute(time(default = now)):
+    returns minute as text format 'MM'
+
+    get_weekday(date(default = now)):
+    returns weekday as text example 'Monday'
+
+    get_weeknum(date(default = now)):
+    returns weeknumber as int
+
+    get_month_name(date(default = now)):
+    returns month as text example 'January'
+
+    add_years(years_to_add, date(default = now)):
+    returns date add years to add as text format 'DD:MM:YYYY'
+
+    add_months(months_to_add, date(default = now)):
+    returns  date add months to add as text format 'DD:MM:YYYY'
+
+    add_days(days_to_add, date(default = now)):
+    returns date add days to add as text format 'DD:MM:YYYY'
+
+    add_hours(hours_to_add, time(default = now)):
+    returns time add hours to add as text format 'DD:MM:YYYY'
+
+    add_minutes(minutes_to_add, time(default = now)):
+    returns time add minutes to add as text format 'DD:MM:YYYY'
+
+    add_seconds(seconds_to_add, dtimedefault = now)):
+    returns time add seconds to add as text format 'DD:MM:YYYY'
---
+++ time_date_util.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+
+def get_now():
+    now = datetime.now()
+    return now
+def get_date():
+    now = get_now()
+    date = now.strftime('%d/%m/%Y')
+    return date
+def get_time():
+    now = get_now()
+    time = now.strftime('%H/%M:%S')
+    return time
+
+
+def get_day(date:datetime = get_now()):
+    day = date.strftime('%d')
+    return day
+def get_month(date:datetime = get_now()):
+    month = date.strftime('%m')
+    return month
+def get_year(date:datetime = get_now()):
+    year = date.strftime('%Y')
+    return year
+
+
+def get_hour(time:datetime = get_now()):
+    hour = time.strftime('%H')
+    return hour
+def get_minute(time:datetime = get_now()):
+    minute = time.strftime('%M')
+    return minute
+
+
+def get_weekday(date:datetime = get_now()):
+    weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    return weekday[datetime.weekday(date)]
+
+def get_month_name(date:datetime = get_now()):
+    months = ['January','Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    month  = int(get_month(date)) - 1
+    return months[month]
+
+def get_weeknum(date:datetime = get_now()):
+    weeknum = date.isocalendar()[1]
+    return int(weeknum)
+
+def add_years(years_to_add, date:datetime = get_now()):
+    newdate = date + relativedelta(years=years_to_add)
+    newdate = newdate.strftime('%d/%m/%Y')
+    return newdate
+def add_months(months_to_add, date:datetime = get_now()):
+    if months_to_add > 12:
+        years = int(months_to_add/12)
+        months_to_add = months_to_add % 12
+        add_years(years,date)
+    newdate = date + relativedelta(months=months_to_add)
+    newdate = newdate.strftime('%d/%m/%Y')
+    return newdate
+def add_days(days_to_add, date:datetime = get_now()):
+    newdate = date + relativedelta(days=days_to_add)
+    newdate = newdate.strftime('%d/%m/%Y')
+    return newdate
+
+
+def add_hours(hours_to_add, time:datetime = get_now()):
+    newtime = time + timedelta(hours=hours_to_add)
+    newtime =newtime.strftime('%H/%M:%S')
+    return newtime
+def add_minutes(minutes_to_add, time:datetime = get_now()):
+    if minutes_to_add > 60:
+        hours =  int(minutes_to_add/60)
+        minutes_to_add = minutes_to_add %60
+        add_hours(hours)
+    newtime = time + timedelta(minutes=minutes_to_add)
+    newtime =newtime.strftime('%H/%M:%S')
+    return newtime
+def add_seconds(seconds_to_add, time:datetime = get_now()):
+    if seconds_to_add > 60:
+        minutes =  int(seconds_to_add/60)
+        seconds_to_add = seconds_to_add %60
+        add_minutes(minutes)
+    newtime = time + timedelta(seconds=seconds_to_add)
+    newtime =newtime.strftime('%H/%M:%S')
+    return newtime
